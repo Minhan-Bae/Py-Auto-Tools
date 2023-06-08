1fa8e3498627475fc4202d2bbc398a6ae4568d40
Minhan-Bae <yuyuqw92@gmail.com>
2023-06-08 10:35:39 +0900

    feat: add directory module

--- generate_init_files.py
+++ generate_init_files.py
@@ -1,12 +1,3 @@
-"""
-This script creates __init__.py files for all directories and subdirectories
-in the specified repository path. For each __init__.py file, it also adds
-import statements for all the Python modules in the same directory.
-Please note that this approach can lead to complications when managing
-complex package structures. Modify the script as needed before using it
-in a real project.
-"""
-
 import os
 import sys
 
@@ -14,13 +5,13 @@ def create_init_py(path):
     """
     Create __init__.py files in all directories and subdirectories
     within the given path, and add import statements for all Python modules
-    in the same directory.
+    and subpackages in the same directory.
 
     Args:
         path (str): The root directory to start creating __init__.py files from.
     """
     # Iterate through all directories and files in the given path.
-    for root, _, files in os.walk(path):
+    for root, dirs, files in os.walk(path):
         init_path = os.path.join(root, "__init__.py")
 
         # Create a new __init__.py file.
@@ -33,6 +24,11 @@ def create_init_py(path):
                 if file.endswith(".py") and file != "__init__.py":
                     module_name = file[:-3]  # Remove '.py' extension
                     init_file.write(f"from . import {module_name}\n")
+
+            # Import all subpackages in the current directory.
+            for dir in sorted(dirs):
+                if any(f.endswith('.py') for f in os.listdir(os.path.join(root, dir))):
+                    init_file.write(f"from . import {dir}\n")
 
             print(f"Created: {init_path}")
 
