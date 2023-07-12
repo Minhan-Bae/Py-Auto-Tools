6b830934d71d988db856590eb0bb8640e592898f
Minhan-Bae <yuyuqw92@gmail.com>
2023-07-12 18:07:06 +0900

    feat: update delete_DS.py 에 argv 기능과 ._파일 제거 기능 추가

--- delete_DS.py
+++ delete_DS.py
@@ -1,12 +1,13 @@
 # delete DS_Store from dir
 import os
-
+import sys
 def main():
-    root = os.getcwd()
+    root = sys.argv[1]
     for dir, _, files in os.walk(root):
         for file in files:
             if file.endswith(".DS_Store"):
                 os.remove(os.path.join(dir,file))
-
+            elif '._' in file:
+                os.remove(os.path.join(dir,file))
 if __name__=="__main__":
     main()
